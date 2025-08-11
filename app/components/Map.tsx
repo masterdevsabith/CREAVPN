ff2047cc0b032cbe3b853c38bd963ec38d121c86
Sabith <masterdevsabith@gmail.com>
2025-08-11 16:43:14 +0530

    map section responsiveness done

--- app/components/Map.tsx
+++ app/components/Map.tsx
@@ -15,40 +15,40 @@ export default function Map() {
           perspiciatis ad ex quis.
         </p>
       </div>
-      <section className="world_map py-14 px-20 h-160 relative">
+      <section className="world_map py-14 px-20 h-140 relative">
         <FloatingFlag
           image="/country-svgs/RU.svg"
           country="Russia"
           servers="45"
-          classname="top-30 right-280"
+          classname="top-30 2xl:right-280 xl:right-240 lg:right-200 md:right-140 sm:right-100"
           alignment="right"
         />
         <FloatingFlag
           image="/country-svgs/PT.svg"
           country="Portugal"
           servers="102"
-          classname="top-80 right-210"
+          classname="top-80 2xl:right-210 xl:right-180 lg:right-140 md:right-120 sm:right-100"
           alignment="right"
         />{" "}
         <FloatingFlag
           image="/country-svgs/US.svg"
           country="USA"
           servers="2"
-          classname="top-30 right-170"
+          classname="top-30 2xl:right-170 xl:right-120 lg:right-90 md:right-60 sm:top-40 sm:right-50"
           alignment="right"
         />
         <FloatingFlag
           image="/country-svgs/ID.svg"
           country="Indonesia"
           servers="89"
-          classname="top-68 right-140"
+          classname="top-68 2xl:right-140 xl:right-90 lg:right-50 md:right-30 sm:right-10"
           alignment="right"
         />
         <FloatingFlag
           image="/country-svgs/MY.svg"
           country="Malaysia"
           servers="16"
-          classname="top-20 right-80"
+          classname="top-20 2xl:right-80 xl:right-40 lg:right-20 md:right-8 sm:right-3"
           alignment="right"
         />
       </section>
--- app/page.tsx
+++ app/page.tsx
@@ -21,8 +21,8 @@ export default function Home() {
         <Companies />
         <SecureBrowsing />
         <InnovativeFeature />
-        {/* <Map /> */}
-        {/* <Pricing /> */}
+        <Map />
+        <Pricing />
         {/* <Faq /> */}
         {/* <Banner /> */}
         {/* <Footer /> */}
