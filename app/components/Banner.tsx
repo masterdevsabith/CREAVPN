d0452417045f0e8120b1e04a5ead15642e81965a
Sabith <masterdevsabith@gmail.com>
2025-08-12 19:26:11 +0530

    banner section responsiveness done

--- app/components/Banner.tsx
+++ app/components/Banner.tsx
@@ -2,14 +2,14 @@ import Button from "./ui/Button";
 
 export default function Banner() {
   return (
-    <section className="banner_sec px-30 py-20 flex items-center justify-between">
-      <div className="left w-2/5">
-        <h2 className="text-5xl text-white font-light text-left">
+    <section className="banner_sec px-30 py-20 flex items-center justify-between lg:flex-row sm:flex-col ">
+      <div className="left xl:w-2/5 lg:w-3/5 sm:mb-5 lg:mb-0">
+        <h2 className="text-5xl text-white font-light lg:text-left sm:text-center">
           Experience Ultimate Online Protection
         </h2>
       </div>
-      <div className="right w-2/5">
-        <p className="mb-12 text-neutral-300 max-w-md">
+      <div className="right xl:w-2/5 lg:block sm:flex sm:flex-col sm:items-center">
+        <p className="lg:mb-12 sm:mb-4 text-neutral-300 max-w-md sm:text-center lg:text-left ">
           Stay safe online with our advanced VPN technology, designed to protect
           your data and privacy no matter where you are.
         </p>
--- app/page.tsx
+++ app/page.tsx
@@ -24,8 +24,8 @@ export default function Home() {
         <Map />
         <Pricing />
         <Faq />
-        {/* <Banner /> */}
-        {/* <Footer /> */}
+        <Banner />
+        <Footer />
       </section>
     </main>
   );
