a3974cadba56ee989af9e03595c55fe587590364
Sabith <masterdevsabith@gmail.com>
2025-08-19 16:48:13 +0530

    successfully implemented leaflet

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,3 +1,11 @@
+import LeafletMap from "./components/LeafLetMap";
+
 export default function Dashboard() {
-  return <h1>welcome to dashboa</h1>;
+  return (
+    <section className="dashboard relative  h-screen overflow-hidden ">
+      <LeafletMap />
+      <div className="left absolute z-30 top-4 left-4 w-80 h-175 bg-white rounded-md"></div>
+      <div className="right absolute z-30 top-2 right-2 w-50 h-100 bg-white rounded-md"></div>
+    </section>
+  );
 }
