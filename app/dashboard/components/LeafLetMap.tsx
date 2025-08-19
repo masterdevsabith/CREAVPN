f258b9d9ee34c09ce5057e8fcd67e35441c44156
Sabith <masterdevsabith@gmail.com>
2025-08-19 16:57:01 +0530

    now map marker is also seen

--- app/dashboard/components/LeafLetMap.tsx
+++ app/dashboard/components/LeafLetMap.tsx
@@ -1,12 +1,46 @@
 "use client";
 
-import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import {
+  MapContainer,
+  TileLayer,
+  Marker,
+  Popup,
+  useMapEvents,
+} from "react-leaflet";
 import "leaflet/dist/leaflet.css";
+import { useState } from "react";
 
 export default function LeafletMap() {
+  L.Icon.Default.mergeOptions({
+    iconRetinaUrl: "/leaflet/marker-icon-2x.png",
+    iconUrl: "/leaflet/marker-icon.png",
+    shadowUrl: "/leaflet/marker-shadow.png",
+  });
+
+  function LocationMarker() {
+    const [position, setPosition] = useState(null);
+    const map = useMapEvents({
+      click() {
+        map.locate();
+      },
+      locationfound(e) {
+        setPosition(e.latlng);
+        map.flyTo(e.latlng, map.getZoom());
+      },
+    });
+
+    return position === null ? null : (
+      <Marker position={position}>
+        <Popup>You are here</Popup>
+      </Marker>
+    );
+  }
+
+  const position = [51.505, -0.09];
+
   return (
     <MapContainer
-      center={[51.505, -0.09]}
+      center={position}
       zoom={13}
       scrollWheelZoom={false}
       className="w-full h-full z-1"
@@ -15,11 +49,13 @@ export default function LeafletMap() {
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
-      <Marker position={[51.505, -0.09]}>
+      <Marker position={position}>
         <Popup>
           A pretty CSS3 popup. <br /> Easily customizable.
         </Popup>
       </Marker>
+
+      <LocationMarker />
     </MapContainer>
   );
 }
--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -4,8 +4,8 @@ export default function Dashboard() {
   return (
     <section className="dashboard relative  h-screen overflow-hidden ">
       <LeafletMap />
-      <div className="left absolute z-30 top-4 left-4 w-80 h-175 bg-white rounded-md"></div>
-      <div className="right absolute z-30 top-2 right-2 w-50 h-100 bg-white rounded-md"></div>
+      {/* <div className="left absolute z-30 top-4 left-4 w-80 h-175 bg-white rounded-md"></div>
+      <div className="right absolute z-30 top-2 right-2 w-50 h-100 bg-white rounded-md"></div> */}
     </section>
   );
 }
